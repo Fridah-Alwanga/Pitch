ccba08a8a0f1cb539b409dc5934f508f905eab80
Fridah-Alwanga <fridahalwanga6@gmail.com>
2020-12-08 06:36:58 +0300

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 export SECRET_KEY= 'fri'
 export MAIL_USERNAME='fridahalwanga6@gmail.com'
 export MAIL_PASSWORD='38642204'
-# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:123456@localhost/pitch'
 
 python manage.py server
